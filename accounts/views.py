7c04222669300a84113c697b91af290507a57730
dashney22 <dashneymakgafela@gmail.com>
2023-05-22 02:34:00 +0200

    Initialized User Registration Functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def login_view(request):
@@ -21,3 +22,16 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect(reverse_lazy('home'))
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login') #If data supplied is in the correct format, then we send you to login page
+        else:
+            messages.error(request, "Invalid User Registration data")
+            return redirect('register')
+    else:
+        form = UserCreationForm()  # Give the user an empty form since they did not post anything
+        return redirect(request,"register.html",{'form':form})
